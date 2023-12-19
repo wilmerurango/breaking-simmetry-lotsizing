f7e996d1691b028c8c725a9a22296df7e41ea53c
Sabrina <sabrinaferreira123@hotmail.com>
2023-12-19 22:15:15 +0000

    ordenamento de acordo com o setup

--- read_file.py
+++ read_file.py
@@ -45,7 +45,7 @@ class LerDados:
         self.sc = context.custo_setup * np.array(df.iloc[inicio:fim, 3].astype(float), dtype=int)
         inicio, fim = fim, fim + self.nperiodos
         if self.nitems <= 15:
-            self.d = np.array(df.iloc[inicio:fim, :].astype(float), dtype=int).T
+            self._d_no_sorted = np.array(df.iloc[inicio:fim, :].astype(float), dtype=int).T
         else:
             demanda_sup = np.array(df.iloc[inicio:fim, :].astype(float), dtype=int)
             inicio, fim = fim, fim + self.nperiodos
@@ -56,7 +56,17 @@ class LerDados:
                 .astype(float),
                 dtype=int,
             )
-            self.d = np.append(demanda_sup, demanda_inf, axis=1).T
+            self._d_no_sorted = np.append(demanda_sup, demanda_inf, axis=1).T
+        self._sort_index()
+
+    def _sort_index(self):
+        sorted_indices = sorted(range(len(self.st)), key=lambda i: self.st[i], reverse=True)
+        # Ordenar o vetor st em ordem decrescente e os demais vetores de acordo com a ordem dos itens
+        self.st = np.array([self.st[i] for i in sorted_indices]) 
+        self.vt = np.array([self.vt[i] for i in sorted_indices])
+        self.hc = np.array([self.hc[i] for i in sorted_indices])
+        self.sc = np.array([self.sc[i] for i in sorted_indices])
+        self.d = np.array([self._d_no_sorted[i] for i in sorted_indices])
 
     def __str__(self) -> str:
         return f"{self.instance}_cap_{self.cap[0]}".replace(".dat", "").replace(".DAT", "")
