6be3bb354035494222afa499aaae7ced2221e20a
Sabrina <sabrinaferreira123@hotmail.com>
2024-01-22 23:02:14 +0000

    definindo instancias

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 # INSTANCES = [f"F{i}.DAT" for i in range(1, 71)] + [f"G{i}.DAT" for i in range(1, 76)]
-INSTANCES = [#"F1.DAT",
-              #"F25.DAT", 
-              #"G59.DAT", 
-             "G64.DAT"
+INSTANCES = ["F1.DAT",
+              "F25.DAT", 
+              "G59.DAT", 
+              "G64.DAT"
              ]
 MAQUINAS = [2,4]
 NUM_POINTS = 10
 FAST_TIMELIMIT = 20
-TIMELIMIT = 300
+TIMELIMIT = 3600
 
 CAPACIDADES_PATH = Path.resolve(Path.cwd() / "resultados" / "capacidades.xlsx")
 RESULTADOS_INDIVIDUAIS_PATH = Path.resolve(Path.cwd() / "resultados" / "individuais")
