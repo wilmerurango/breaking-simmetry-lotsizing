a390f42ba6e4cd091b1c858df2088b17e628dc5e
Sabrina <sabrinaferreira123@hotmail.com>
2024-01-23 16:46:47 +0000

    alterando instancia para refazer o teste G59

--- constants.py
+++ constants.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 
-INSTANCES = [f"F{i}.DAT" for i in range(1, 71)] + [f"G{i}.DAT" for i in range(1, 76)]
-MAQUINAS = [2,4,6]
+# INSTANCES = [f"F{i}.DAT" for i in range(1, 71)] + [f"G{i}.DAT" for i in range(1, 76)]
+INSTANCES = [#"F1.DAT",
+              #"F25.DAT", 
+              "G59.DAT", 
+              #"G64.DAT"
+             ]
+MAQUINAS = [2]
 NUM_POINTS = 10
 FAST_TIMELIMIT = 20
-TIMELIMIT = 3600
+TIMELIMIT = 7200
 
 CAPACIDADES_PATH = Path.resolve(Path.cwd() / "resultados" / "capacidades.xlsx")
 RESULTADOS_INDIVIDUAIS_PATH = Path.resolve(Path.cwd() / "resultados" / "individuais")
 OTIMIZADOS_INDIVIDUAIS_PATH = Path.resolve(Path.cwd() / "resultados" / "otimizados")
+DETALHADOS_INDIVIDUAIS_PATH = Path.resolve(Path.cwd() / "resultados" / "detalhados")
 FINAL_PATH = Path.resolve(Path.cwd() / "resultados")
 
 IDEAL_CAPACITY = 75
