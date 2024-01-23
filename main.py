7c467b007c3258872646763fa72eaae6ec39bc62
Sabrina <sabrinaferreira123@hotmail.com>
2024-01-23 22:00:31 +0000

    retornando o main para os testes com 3 formulações

--- main.py
+++ main.py
@@ -4,36 +4,40 @@ from utils import (
 )
 from context import ProjectContext
 from zero_formulation import build_model as classical_formulation_build_model
-from first_formulation import build_model as first_reformulation_build_model
-from second_formulation import build_model as second_reformulation_build_model
+from first_formulation import build_model as first_formulation_build_model
+from second_formulation import build_model as second_formulation_build_model
+from third_formulation import build_model as third_formulation_build_model
 
 if __name__ == "__main__":
-    for num in [1]:
-        context = ProjectContext(f"experimentos/experimento{num}.yml", num)
-        running_all_instance_choose_capacity(
-            context,
-            classical_formulation_build_model,
-        )
+    # for num in [1]:
+    context = ProjectContext(f"experimentos/experimento1.yml", 1)
+    #     running_all_instance_choose_capacity(
+    #         context,
+    #         classical_formulation_build_model,
+    #     )
     # running_all_instance_with_chosen_capacity(
     #     classical_formulation_build_model,
     #     path_to_save="otimizados0.xlsx",
     #     env_formulation="0st_ref",
     # )
-    # running_all_instance_with_chosen_capacity(
-    #     first_reformulation_build_model,
-    #     path_to_save="otimizados1.xlsx",
-    #     env_formulation="1st_ref",
-    # )
-    # running_all_instance_with_chosen_capacity(
-    #     second_reformulation_build_model,
-    #     path_to_save="otimizados2.xlsx",
-    #     env_formulation="2nd_ref",
-    # )
-    # running_all_instance_with_chosen_capacity(
-    #     third_reformulation_build_model,
-    #     path_to_save="otimizados3.xlsx",
-    #     env_formulation="3nd_ref",
-    # )
+    running_all_instance_with_chosen_capacity(
+        context,
+        first_formulation_build_model,
+        path_to_save=f"otimizados_1_experiment_1.xlsx",
+        env_formulation="1_ref",
+    )
+    running_all_instance_with_chosen_capacity(
+        context,
+        second_formulation_build_model,
+        path_to_save=f"otimizados_2_experiment_1.xlsx",
+        env_formulation="2_ref",
+    )
+    running_all_instance_with_chosen_capacity(
+        context,
+        third_formulation_build_model,
+        path_to_save=f"otimizados_3_experiment_1.xlsx",
+        env_formulation="3_ref",
+    )
     # running_all_instance_with_chosen_capacity(
     #     fourth_reformulation_build_model,
     #     path_to_save="otimizados4.xlsx",
