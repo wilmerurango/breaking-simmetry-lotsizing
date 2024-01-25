cbbd9fc85f449510517a0a15bac27b0900faab14
Sabrina <sabrinaferreira123@hotmail.com>
2024-01-25 10:20:19 +0000

    alteração das instancias para teste

--- constants.py
+++ constants.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 # INSTANCES = [f"F{i}.DAT" for i in range(1, 71)] + [f"G{i}.DAT" for i in range(1, 76)]
 INSTANCES = ["F1.DAT",
-              #"F25.DAT", 
+              "F25.DAT", 
               "G59.DAT", 
               "G64.DAT"
              ]
-MAQUINAS = [4]
+MAQUINAS = [2,4]
 NUM_POINTS = 10
 FAST_TIMELIMIT = 20
 TIMELIMIT = 3600
