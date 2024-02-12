8ac39345c6b675991c3c5f94891e17a99126e637
Sabrina <sabrinaferreira123@hotmail.com>
2024-02-12 17:40:09 +0000

    mudança no openmpi

--- script_memshort.sh
+++ script_memshort.sh
@@ -7,7 +7,7 @@
 #PBS -M s209376@dac.unicamp.br
 
 module load python/3.8.11-intel-2021.3.0
-module load openmpi/4.1.1-intel-2021.3.0
+module load openmpi/4.1.1-gcc-9.4.0
 source py38/bin/activate
 
 cd $PBS_O_WORKDIR
