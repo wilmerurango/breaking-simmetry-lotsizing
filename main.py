aa41e43b9da6de67b33ac8bd5acb200789049ff1
Sabrina <sabrinaferreira123@hotmail.com>
2024-04-03 23:08:24 -0300

    novo teste com F0

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     #         context,
     #         classical_formulation_build_model,
     #     )
-    # running_all_instance_with_chosen_capacity(
-    #     context,
-    #     classical_formulation_build_model,
-    #     path_to_save="otimizados_0_experiment_1.xlsx",
-    #     env_formulation="0_ref",
-    # )
+    running_all_instance_with_chosen_capacity(
+        context,
+        classical_formulation_build_model,
+        path_to_save="otimizados_0_experiment_1.xlsx",
+        env_formulation="0_ref",
+    )
     # running_all_instance_with_chosen_capacity(
     #     context,
     #     first_formulation_build_model,
@@ -49,12 +49,12 @@ if __name__ == "__main__":
     #     path_to_save="otimizados_4_experiment_1.xlsx",
     #     env_formulation="4_ref",
     # )
-    running_all_instance_with_chosen_capacity(
-        context,
-        fifth_formulation_build_model,
-        path_to_save="otimizados_5_experiment_1.xlsx",
-        env_formulation="5_ref",
-    )
+    # running_all_instance_with_chosen_capacity(
+    #     context,
+    #     fifth_formulation_build_model,
+    #     path_to_save="otimizados_5_experiment_1.xlsx",
+    #     env_formulation="5_ref",
+    # )
     # running_all_instance_with_chosen_capacity(
     #     context,
     #     sixth_formulation_build_model,
