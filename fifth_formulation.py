b32b1a2980c649e258cffc68115ed6ef252f5c49
Sabrina <sabrinaferreira123@hotmail.com>
2024-04-30 16:30:41 -0300

    formulação F2 com teste de nova quebra de simetria de máquinas

--- fifth_formulation.py
+++ fifth_formulation.py
@@ -1,10 +1,7 @@
-from typing import Dict, List
-
+from typing import Dict
 from docplex.mp.model import Model
 
 from read_file import dataCS
-from utils import cs_aux
-
 
 def create_variables(mdl: Model, data: dataCS) -> Model:
     mdl.z = mdl.binary_var_dict(
@@ -29,6 +26,17 @@ def create_variables(mdl: Model, data: dataCS) -> Model:
         ub=1,
         name=f"w",
     )
+    mdl.x = mdl.continuous_var_dict(
+        (
+            (i, j, t, k)
+            for i in range(data.nitems)
+            for j in range(data.r)
+            for t in range(data.nperiodos)
+            for k in range(data.nperiodos)
+        ),
+        lb=0,
+        name=f"x",
+    )
     mdl.f = mdl.continuous_var_dict(
         (
             (i, j, t)
@@ -49,31 +57,17 @@ def create_variables(mdl: Model, data: dataCS) -> Model:
         lb=0,
         name=f"l",
     )
-    mdl.z = mdl.continuous_var(lb=0, name=f"u")
-    
-    mdl.x = mdl.continuous_var_dict(
-        (
-            (i, j, t, k)
-            for i in range(data.nitems)
-            for j in range(data.r)
-            for t in range(data.nperiodos)
-            for k in range(data.nperiodos)
-        ),
-        lb=0,
-        ub=1,
-        name=f"x",
-    )
     return mdl
 
+
 def define_obj_function(mdl: Model, data: dataCS) -> Model:
-    # cs_aux = cs_aux(data)
     mtd_func = mdl.sum(
-        data.sc[i] * (mdl.z[i, j, t]+ mdl.w[i, j, t])
+        data.sc[i] * (mdl.z[i, j, t] + mdl.w[i, j, t])
         for i in range(data.nitems)
         for j in range(data.r)
         for t in range(data.nperiodos)
     ) + sum(
-        cs_aux(data)[i, j, t, k] * mdl.x[i, j, t, k]
+        data.cs[i, t, k] * mdl.x[i, j, t, k]
         for i in range(data.nitems)
         for j in range(data.r)
         for t in range(data.nperiodos)
@@ -126,11 +120,14 @@ def constraint_setup(mdl: Model, data: dataCS) -> Model:
     return mdl
 
 def constraint_split_time(mdl: Model, data: dataCS) -> Model:
-    mdl.add_constraints(
-        mdl.f[i, j, t] + mdl.l[i,j,t-1] == mdl.w[i, j, t] * data.st[i]
-        for i in range(data.nitems)
-        for j in range(data.r)
-        for t in range(data.nperiodos)
+    for i in range(data.nitems):
+        for j in range(data.r):
+            mdl.add_constraint(
+                    mdl.f[i, j, 0] == mdl.w[i, j, 0] * data.st[i])
+            for t in range(1,data.nperiodos):
+                mdl.add_constraint(
+                    mdl.f[i, j, t] + mdl.l[i,j,t-1] == mdl.w[i, j, t] * data.st[i]
+        
     )
     return mdl
 
@@ -141,25 +138,19 @@ def constraint_split_max(mdl: Model, data: dataCS) -> Model:
         for t in range(data.nperiodos)
     )
     return mdl
-    
 
-def valor_funcao_obj(mdl: Model, data: dataCS) -> Model:
-    return sum(
-            data.sc[i] * mdl.y[i, j, t]
-            for i in range(data.nitems)
-            for j in range(data.r)
-            for t in range(data.nperiodos)
-        ) + sum(
-            data.cs[i, t, k] * mdl.x[i, j, t, k]
-            for i in range(data.nitems)
-            for j in range(data.r)
-            for t in range(data.nperiodos)
-            for k in range(t, data.nperiodos)
+def constraint_symmetry_breaking(mdl: Model, data: dataCS) -> Model:
+    mdl.add_constraints(
+        (-1)**(t+1)*mdl.sum(data.d[i,k]*mdl.x[i,j-1,t,k] for i in range(data.nitems) for k in range(t,data.nperiodos)) >= 
+                              (-1)**(t+1)*mdl.sum(data.d[i,k]*mdl.x[i,j,t,k] for i in range(data.nitems) for k in range(t,data.nperiodos))
+        for j in range(1,data.r)
+        for t in range(data.nperiodos)
     )
+    return mdl
 
 def total_setup_cost(mdl, data):
     return sum(
-        data.sc[i] * mdl.y[i, j, t]
+        data.sc[i] * (mdl.z[i, j, t]+mdl.w[i, j, t])
         for i in range(data.nitems)
         for j in range(data.r)
         for t in range(data.nperiodos)
@@ -178,7 +169,7 @@ def total_estoque_cost(mdl, data):
 
 def used_capacity(mdl, data):
     return sum(
-        data.st[i] * mdl.y[i, j, t]
+        data.st[i] * mdl.z[i, j, t]+ mdl.l[i, j, t] + mdl.f[i,j,t]
         for i in range(data.nitems)
         for j in range(data.r)
         for t in range(data.nperiodos)
@@ -193,31 +184,40 @@ def used_capacity(mdl, data):
 
 def total_y(mdl, data):
     return sum(
-        mdl.y[i, j, t]
+        mdl.z[i, j, t]+mdl.w[i, j, t]
         for i in range(data.nitems)
         for j in range(data.r)
         for t in range(data.nperiodos)
     )
 
 
-def closest_to_75_percent(results_per_instance: List[Dict[str, any]]) -> Dict[str, any]:
-    """Dado uma lista de resultados para uma instância, retorne aquele mais próximo de 75% de utilização da capacidade."""
-    return min(results_per_instance, key=lambda x: abs(x["utilization_capacity"] - 75))
+def add_new_kpi(kpis: Dict[str, any], result, data: dataCS) -> dict:
+    kpis["Instance"] = data.instance
+    kpis["Best Bound"] = result.solve_details.best_bound
+    kpis["Gap"] = result.solve_details.gap
+    kpis["Nodes Processed"] = result.solve_details.gap
+    kpis["Tempo de Solução"] = result.solve_details.time
+    kpis["capacity"] = data.cap[0]
+    kpis["utilization_capacity"] = (
+        100 * kpis.get("used_capacity", 0) / (data.cap[0] * data.r * data.nperiodos)
+    )
+    kpis["nmaquinas"] = data.r
+    return kpis
 
 
-def build_model(data: dataCS, capacity: float) -> Model:
+def build_model(data: dataCS, capacity: float) -> Model:    
     data.cap[0] = capacity
     mdl = Model(name="mtd")
-    mdl.context.cplex_parameters.threads = 1
     mdl = create_variables(mdl, data)
     mdl = define_obj_function(mdl, data)
     mdl = constraint_demanda_satisfeita(mdl, data)
     mdl = constraint_capacity(mdl, data)
     mdl = constraint_setup(mdl, data)
-
+    mdl = constraint_split_time(mdl, data)
+    mdl = constraint_split_max(mdl, data)
+    mdl = constraint_symmetry_breaking(mdl, data)
     mdl.add_kpi(total_setup_cost(mdl, data), "total_setup_cost")
     mdl.add_kpi(total_estoque_cost(mdl, data), "total_estoque_cost")
     mdl.add_kpi(used_capacity(mdl, data), "used_capacity")
     mdl.add_kpi(total_y(mdl, data), "total_y")
-    mdl.add_kpi(valor_funcao_obj(mdl, data), "valor_funcao_obj")
     return mdl, data
