cc6d7d98675ded57b5b20437d57cf8ac950db3af
Sabrina <sabrinaferreira123@hotmail.com>
2024-05-19 13:58:34 -0300

    correção no constants

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 4) for j in range(1,3) for k in range(1,5) 
+INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 4) for j in range(1,3) for k in [1,2,4] 
              for l in range(1,3) for m in range(7,10) for n in ["A", "B", "C", "D", "E"]] 
 # + [f"G{i}.DAT" for i in range(1, 76)]
 #INSTANCES = [#"F1.DAT",
