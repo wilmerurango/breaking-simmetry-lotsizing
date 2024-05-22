86d893af2a093f58896b7823dc8fd6f4fb5b3a23
Sabrina <sabrinaferreira123@hotmail.com>
2024-05-22 16:23:50 -0300

    encontrar as capacidades das instancias que faltaram

--- constants.py
+++ constants.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
 
-INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 4) for j in range(1,3) for k in [1,2,4] 
+INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 2) for j in range(1,2) for k in [1,2] 
              for l in range(1,3) for m in range(7,10) for n in ["A", "B", "C", "D", "E"]] 
-# + [f"G{i}.DAT" for i in range(1, 76)]
++ [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 2) for j in range(1,2) for k in [4] 
+             for l in range(1,2) for m in range(7,9) for n in ["A", "B", "C", "D", "E"]]
 #INSTANCES = [#"F1.DAT",
               #"F25.DAT", 
               #"G59.DAT", 
               #"G64.DAT"]
 MAQUINAS = [2,4,6]
-NUM_POINTS = 20
+NUM_POINTS = 15
 FAST_TIMELIMIT = 20
 TIMELIMIT = 3600
 
