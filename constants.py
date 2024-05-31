f5cbdbc19918b95618cdbc2e852ac2592dacb19a
Sabrina <sabrinaferreira123@hotmail.com>
2024-05-31 11:20:15 -0300

    teste instancia 1

--- constants.py
+++ constants.py
@@ -4,14 +4,11 @@ from pathlib import Path
 INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 2) for j in range(1,2) for k in [1,2] 
              for l in range(1,3) for m in range(7,10) for n in ["A", "B", "C", "D", "E"]] + [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 2) for j in range(1,2) for k in [4] 
              for l in range(1,2) for m in range(7,9) for n in ["A", "B", "C", "D", "E"]]
-#INSTANCES = [#"F1.DAT",
-              #"F25.DAT", 
-              #"G59.DAT", 
-              #"G64.DAT"]
-MAQUINAS = [2,4,6]
+INSTANCES = ["X11117A.DAT"]
+MAQUINAS = [2]
 NUM_POINTS = 15
 FAST_TIMELIMIT = 20
-TIMELIMIT = 3600
+TIMELIMIT = 10
 
 CAPACIDADES_PATH = Path.resolve(Path.cwd() / "resultados" / "capacidades.xlsx")
 RESULTADOS_INDIVIDUAIS_PATH = Path.resolve(Path.cwd() / "resultados" / "individuais")
