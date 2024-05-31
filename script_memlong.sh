8a9f78d58efa4e0d726d446baff3f87ba44e184c
Gabriel Amaro <gabriel.amaro94@gmail.com>
2024-05-31 16:47:39 -0300

    alterado config do scripts para nova conf do mpi4py

--- script_memlong.sh
+++ script_memlong.sh
@@ -6,9 +6,9 @@
 #PBS -m abe
 #PBS -M s209376@dac.unicamp.br
 
-module load python/3.8.11-intel-2021.3.0
-module load openmpi/4.1.1-gcc-9.4.0
-source py38/bin/activate
+module load anaconda3
+module load mpich/4.1.1-gcc-9.4.0
+source newmpi/bin/activate
 
 cd $PBS_O_WORKDIR
 mpirun -np 128 python -m mpi4py.futures main.py
