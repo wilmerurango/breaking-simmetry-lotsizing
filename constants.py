80ed1c620d4052c9d209b3cc481e12529e0703fd
Sabrina <sabrinaferreira123@hotmail.com>
2024-06-06 13:41:16 -0300

    correção do .DAT em instancias

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.dat" for i in range(1, 4) for j in range(1,3) for k in [1,2,4] 
+INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 4) for j in range(1,3) for k in [1,2,4] 
              for l in range(1,3) for m in range(7,10) for n in ["A", "B", "C", "D", "E"]]
 
 MAQUINAS = [2,4,6]
