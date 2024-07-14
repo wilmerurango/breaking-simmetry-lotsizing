f6cc1f3069fe1b9c94e3f2309886cbec9fb77c52
Sabrina <sabrinaferreira123@hotmail.com>
2024-07-14 20:01:16 -0300

    alterações para experimento 3.1

--- constants.py
+++ constants.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
 
-INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 2) for j in range(1,2) for k in [1,2] 
-             for l in range(1,3) for m in range(7,10) for n in ["A", "B", "C", "D", "E"]] + [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 2) for j in range(1,2) for k in [4] 
-             for l in range(1,2) for m in range(7,9) for n in ["A", "B", "C", "D", "E"]]
+INSTANCES = [f"X{i}{j}{k}{l}{m}{n}.DAT" for i in range(1, 2) for j in range(1,3) for k in [1,2,4] 
+             for l in range(1,3) for m in range(7,10) for n in ["A", "B", "C", "D", "E"]]
 
 MAQUINAS = [2,4,6]
 NUM_POINTS = 15
